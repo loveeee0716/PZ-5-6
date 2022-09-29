47738664e02bc9b0852b559ec2051b292e3e37d8
Павел Левченков <levchenkov.levp@mail.ru>
2022-09-29 11:33:16 +0500

    A

---
+++ PZ 6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PZ_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение t: ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            double y;
+            double x = -2;
+            while (x <= 12)
+            {
+                if (x < 0)
+                    y = t;
+                else if (0 <= x && x < 10)
+                    y = t * x;
+                else
+                    y = 2 * t;
+                x += 2;
+                Console.WriteLine(y);
+            }
+    }
+}
